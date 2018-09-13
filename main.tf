0c41946031ab34476b9a3d685e7b8583a4ba3ccd
neogabe <gadaro@consultant.com>
2018-09-13 11:48:13 +0200

    Added KayPair name

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-06340c8c12baa6a09"
     instance_type = "t2.micro"
+    key_name = "terraform-key"
 
     tags {
       Name = "terraform-example"
