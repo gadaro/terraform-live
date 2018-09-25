e69094c16dc731cad742fd6ce43bab4ceaa24cbc
neogabe <gadaro@consultant.com>
2018-09-25 17:06:00 +0200

    If/else testing

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -13,6 +13,11 @@ resource "aws_iam_user" "example" {
     name  = "${element(var.user_names, count.index)}"
 }
 
+resource "aws_iam_policy" "ec2_read_only" {
+    name   = "ec2-read-only"
+    policy = "${data.aws_iam_policy_document.ec2_read_only.json}"
+}
+
 data "aws_iam_policy_document" "ec2_read_only" {
   statement {
     effect    = "Allow"
@@ -21,13 +26,48 @@ data "aws_iam_policy_document" "ec2_read_only" {
   }
 }
 
-resource "aws_iam_policy" "ec2_read_only" {
-    name   = "ec2-read-only"
-    policy = "${data.aws_iam_policy_document.ec2_read_only.json}"
+resource "aws_iam_policy" "cloudwatch_read_only" {
+    name   = "cloudwatch-read-only"
+    policy = "${data.aws_iam_policy_document.cloudwatch_read_only.json}"
+}
+
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+    statement {
+      effect = "Allow"
+      actions = ["cloudwatch:Describe*", "cloudwatch:Get*", "cloudwatch:List"]
+      resources = ["*"]
+    }
+}
+
+resource "aws_iam_policy" "cloudwatch_full_access" {
+    name   = "cloudwatch-full-access"
+    policy = "${data.aws_iam_policy_document.cloudwatch_full_access.json}"
+}
+
+data "aws_iam_policy_document" "cloudwatch_full_access" {
+    statement {
+      effect = "Allow"
+      actions = ["cloudwatch:*"]
+      resources = ["*"]
+    }
 }
 
 resource "aws_iam_user_policy_attachment" "ec2_access" {
     count      = "${length(var.user_names)}"
     user       = "${element(aws_iam_user.example.*.name, count.index)}"
     policy_arn = "${aws_iam_policy.ec2_read_only.arn}"
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
+    count      = "${var.give_neo_cloudwatch_full_access}"
+    user       = "${aws_iam_user.example.0.name}"
+    policy_arn = "${aws_iam_policy.cloudwatch_full_access.arn}"
+}
+
+/* If var.give_neo_cloudwatch_full_access is true, count will be 0, so no read
+only is applied. Validate fails, and only works when using 1 or 0. */
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+    count      = "${1 - var.give_neo_cloudwatch_full_access}"
+    user       = "${aws_iam_user.example.0.name}"
+    policy_arn = "${aws_iam_policy.cloudwatch_read_only.arn}"
 }
